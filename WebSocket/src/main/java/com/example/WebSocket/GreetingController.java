90b909309e75820c1e2ca72e6819ae7595049274
keerthanaDeviVenkatesan <keerthanadevi.v@marketsimplified.com>
2023-09-29 15:50:13 +0530

    commit changes

---
+++ WebSocket/src/main/java/com/example/WebSocket/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.WebSocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greetings greet(Message message) throws InterruptedException {
+        Thread.sleep(2000);
+        return new Greetings("Hello, " +
+                HtmlUtils.htmlEscape(message.getName()));
+    }
+}
